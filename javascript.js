9c2943bbe9f4ab9d4635ddf892b15868c87cfda8
gldsct <188839581+gldsct@users.noreply.github.com>
2025-05-22 14:12:48 +0545

    Implement functionality to display new books
    Implement functionality to display newly added books' cards.

--- javascript.js
+++ javascript.js
@@ -16,6 +16,51 @@ function addBookToLibrary(newName, newAuthor, newPages, newHasRead) {
     myLibrary.push(newBook);
 }
 
+function displayBooks() {
+    // Select and remove all currently displayed books, to prevent duplicate displays of books
+    let currentBooks = document.querySelectorAll("[data-unique-id]");
+    currentBooks.forEach((item) => {
+        booksContainer.removeChild(item);
+    });
+
+    myLibrary.forEach((item, bookIndex, array) => {
+        // Create card for each book
+        let bookAddition = document.createElement("div");
+        bookAddition.dataset.uniqueId = item.id;
+        bookAddition.style.borderRadius = "12px";
+        bookAddition.style.backgroundColor = "#EFD6B5";
+        bookAddition.style.border = "2px solid #658E58";
+        bookAddition.style.padding = "1.5rem";
+
+        let bookName = document.createElement("p");
+        let bookAuthor = document.createElement("p");
+        let bookPages = document.createElement("p");
+        let bookHasRead = document.createElement("p");
+
+        bookName.textContent = `Name: ${item.name}`;
+        bookAuthor.textContent = `Author: ${item.author}`;
+        bookPages.textContent = `Pages: ${item.pages}`;
+        bookHasRead.textContent = item.hasRead === true? `Read? Read`: `Read? Not Read`; 
+        
+        // Create "Remove Book" button
+        let bookRemove = document.createElement("button");
+        bookRemove.textContent = "Remove Book";
+        bookRemove.addEventListener("click", () => {
+            myLibrary.splice(bookIndex, 1);
+            booksContainer.removeChild(bookAddition);
+        });
+
+        bookAddition.appendChild(bookName);
+        bookAddition.appendChild(bookAuthor);
+        bookAddition.appendChild(bookPages);
+        bookAddition.appendChild(bookHasRead);
+        bookAddition.appendChild(bookRemove);
+        booksContainer.appendChild(bookAddition);
+    });
+}
+
+let booksContainer = document.querySelector(".books-container");
+
 // Create "Add New Book" Button
 let newBookButton = document.querySelector(".new-book-button");
 newBookButton.addEventListener("click", () => {
@@ -39,6 +84,8 @@ modalSubmitButton.addEventListener("click", (event) => {
 
     modal.classList.add("hidden");
     modalOverlay.classList.add("hidden");
+
+    displayBooks();
 });
 
 modalCloseButton.addEventListener("click", () => {
